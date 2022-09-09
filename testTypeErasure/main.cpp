fa8f86cd37503767986182c7a2c3a1f0ddc4405e
xiyang yeh <xiyangyeh@gmail.com>
2022-09-09 16:59:24 -0700

    type erasure

--- testTypeErasure/main.cpp
+++ testTypeErasure/main.cpp
@@ -1,40 +1,54 @@
-#include <Eigen/Dense>
-#include <fstream>
+// typeErasure.cpp
+
 #include <iostream>
-#include <nlohmann/json.hpp>
+#include <memory>
 #include <string>
+#include <vector>
 
-using json = nlohmann::json;
+class Object {  // (2)
 
-const std::string configFile = "../transformation_camera_to_base.json";
+public:
+  template <typename T>  // (3)
+  Object(T&& obj) : object(std::make_shared<Model<T>>(std::forward<T>(obj))) {}
 
-int main(int argc, char* argv[]) {
-  std::ifstream file;
-  file.open(configFile);
-  if (!file.is_open()) {
-    std::cerr << "Configuration file cannot be loaded" << std::endl;
-    return -1;
+  std::string getName() const {  // (4)
+    return object->getName();
   }
 
-  std::string jsonConfig((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-  auto jsonTags = json::parse(jsonConfig);
-  file.close();
+  struct Concept {  // (5) The concept, everything here must be pure virtual
+    virtual ~Concept() {}
+    virtual std::string getName() const = 0;
+  };
 
-  // clang-format off
-  Eigen::Vector3d vec;
-  vec << (jsonTags["bTc"]["pos"]["x"]),
-         (jsonTags["bTc"]["pos"]["y"]),
-         (jsonTags["bTc"]["pos"]["z"]);
-  // clang-format on
+  template <typename T>  // (6) The model is an instantiation of the concept
+  struct Model : Concept {
+    Model(const T& t) : object(t) {}
+    std::string getName() const override { return object.getName(); }
 
-  // clang-format off
-  Eigen::Quaterniond q((jsonTags["bTc"]["quat"]["w"]),
-                       (jsonTags["bTc"]["quat"]["x"]),
-                       (jsonTags["bTc"]["quat"]["y"]),
-                       (jsonTags["bTc"]["quat"]["z"]));
-  // clang-format on
+  private:
+    T object;
+  };
 
-  std::cout << "bTc.p = \n" << vec.transpose() << std::endl;
-  std::cout << "bTc.w = \n" << q.w() << std::endl;
-  std::cout << "bTc.vec = \n" << q.vec().transpose() << std::endl;
+  std::shared_ptr<const Concept> object;
+};
+
+void printName(std::vector<Object> vec) {  // (7)
+  for (auto v : vec) std::cout << v.getName() << '\n';
+}
+
+struct Bar {
+  std::string getName() const {  // (8)
+    return "Bar";
+  }
+};
+
+struct Foo {
+  std::string getName() const {  // (8)
+    return "Foo";
+  }
+};
+
+int main() {
+  std::vector<Object> vec{Object(Foo()), Object(Bar())};  // (1)
+  printName(vec);
 }
